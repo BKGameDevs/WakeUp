3363503531d8499a4a3977f328f322bd107ea43f
Kwan Holloway <kwansholloway@gmail.com>
2021-03-02 20:05:59 -0500

    New Tilemap colors

--- WakeUp/Assets/Scripts/PlayerController.cs
+++ WakeUp/Assets/Scripts/PlayerController.cs
@@ -61,6 +61,10 @@ public class PlayerController : MonoBehaviour
         // Setting bool values with conditionals to check if layer is moving left or right
         // Setting bool values with conditionals to check if layer is moving up or down
         _Horizontal = IsPressingLeft ? -1 : (IsPressingRight ? 1 : 0);
+        if(_Horizontal != 0) {
+            Debug.Log(_Horizontal);
+        }
+        
         _Vertical = IsPressingDown ? -1 : ((IsPressingUp || IsPressingSpace) ? 1 : 0);
 
         _Animator.SetBool("Running", _Horizontal != 0);
