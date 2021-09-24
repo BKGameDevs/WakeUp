dd4c5161318ddecb52c7ea105f11756871333e3d
LoboOfTheAbyss <lobooftheabyss@gmail.com>
2021-09-23 20:00:25 -0400

    UI Menu updates and beginning of Credits Menu using UI Toolkit

--- WakeUp/Assets/Scripts/MainMenuController.cs
+++ WakeUp/Assets/Scripts/MainMenuController.cs
@@ -2,41 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UIElements;
 
 public class MainMenuController : MenuController<MainMenuController>
 {
+    public UnityEvent ResumeClicked;
+    public UnityEvent SettingsClicked;
+    public UnityEvent QuitClicked;
+
+    public MainMenuController()
+    {
+        _DontDestroyOnLoad = false;
+    }
+
     public override void RootInitialized(VisualElement root)
     {
-        var resumeButton = root.Q<Button>("Resume");
+        var resumeButton = root.Q<Button>("Resume_Button");
 
         if (resumeButton != null)
-            resumeButton.clicked += Resume_clicked;
+            resumeButton.clicked += () => ResumeClicked?.Invoke();
 
-        var settingsButton = root.Q<Button>("Settings");
+        var settingsButton = root.Q<Button>("Settings_Button");
 
         if (settingsButton != null)
-            settingsButton.clicked += Settings_Clicked;
+            settingsButton.clicked += () => SettingsClicked?.Invoke();
 
-        var quitButton = root.Q<Button>("Quit");
+        var quitButton = root.Q<Button>("Quit_Button");
 
         if (quitButton != null)
-            quitButton.clicked += Quit_clicked;
-    }
-
-    private void Settings_Clicked()
-    {
-        Close(false);
-        SettingsController.Instance.Open();
-    }
-
-    private void Quit_clicked()
-    {
-#if (!UNITY_EDITOR)
-        Util.Quit();
-#else
-        Close();
-#endif
+            quitButton.clicked += () => QuitClicked?.Invoke();
     }
 
     private void Update()
@@ -45,10 +40,6 @@ public class MainMenuController : MenuController<MainMenuController>
             Open();
     }
 
-    private void Resume_clicked()
-    {
-        Close();
-    }
 
     public void Open(bool pause = true)
     {
--- WakeUp/Assets/Scripts/SettingsController.cs
+++ WakeUp/Assets/Scripts/SettingsController.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
 using UnityEngine.UIElements;
 
 public class SettingsController : MenuController<SettingsController>
 {
     public AudioListener AudioListener;
+
+    public UnityEvent BackClicked;
+
+    public SettingsController()
+    {
+        _DontDestroyOnLoad = false;
+    }
+
 
     public override void RootInitialized(VisualElement root)
     {
@@ -16,16 +25,10 @@ public class SettingsController : MenuController<SettingsController>
         if (volumeSlider != null)
             volumeSlider.RegisterValueChangedCallback(Slider_Changed);
 
-        var backButton = _Root.Q<Button>("Back");
+        var backButton = _Root.Q<Button>("Back_Button");
 
         if (backButton != null)
-            backButton.clicked += Back_Clicked;
-    }
-
-    private void Back_Clicked()
-    {
-        Close();
-        MainMenuController.Instance.Open(false);
+            backButton.clicked += () => BackClicked?.Invoke();
     }
 
     private void Slider_Changed(ChangeEvent<float> evt)
